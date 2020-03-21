f49d2ed964f771d0c1dfe4348ea0101fb2f6c244
Dan <danbeladev@gmail.com>
2020-03-21 02:53:55 +0200

    fronet end posts page

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
+import { PostService } from '../post.service';
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+  enteredValue = '';
+  enteredTitle = '';
+
+
+  constructor(public postService: PostService) {}
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content
+    };
+    this.postService.addPost(form.value.title, form.value.content);
+    form.resetForm();
+  }
+}
